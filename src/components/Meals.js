aad5ff73db43224e29e3a1151767e41f73076600
Kaspar <kylgorvigajava@gmail.com>
2025-04-04 10:52:28 +0300

    Ülesanne 3 - done

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,8 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
 const Meals = () => {
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+
+        fetch("http://localhost:3001/meals")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch meals");
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Meals data: ", data);
+                setMeals(data);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }, []);
     return (
-        <ul id="meals">
-            { 
-                // list of meals
+        <ul>
+            {
+
             }
         </ul>
     )
