d9017f2b2cff7338a1f163e83dc8b08949cc9ccd
Kaspar <kylgorvigajava@gmail.com>
2025-04-04 11:37:47 +0300

    Ülesanne 6 -done

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -2,13 +2,19 @@ import React from "react";
 import "../index.css";
 
 const MealItem = (props) => {
+
+    const priceInEur = new Intl.NumberFormat("et-EE", {
+        style: "currency",
+        currency: "EUR",
+    }).format(props.meal.price);
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name} />
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p>props.meal.price</p>
+                    <p className="meal-item-price">{priceInEur}</p>
                     <p>{props.meal.description}</p>
                 </div>
                 <p>
