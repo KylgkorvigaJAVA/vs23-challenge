f009e0f56adad247e2e3d680d3c448f1d53c2713
Kaspar <kylgorvigajava@gmail.com>
2025-04-11 10:09:36 +0300

    Ülesanne 9 & 10 - done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.jpg';
 import "../index.css";
 import Button from "./UI/Button.js";
 import { useCart } from "../store/CartContext";
+import CartModal from "./UI/Modal";
 
 const Header = () => {
     const { cartItems } = useCart();
+    const [cartIsOpen, setCartIsOpen] = useState(false);
 
     const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
 
+    const handleCartClick = () => {
+        if (cartItems.length > 0) {
+            setCartIsOpen(true);
+        }
+    }
+
     return (
-        <header id="main-header">
-            <div id="title">
-                <img src={logo} />
-                <h1>React Food Order App</h1>
-            </div>
-            <nav>
-                <Button textOnly={true}>
-                    Cart ({totalQuantity})
-                </Button>
-            </nav>
-        </header>
+        <>
+            <CartModal isOpen={cartIsOpen} onClose={() => setCartIsOpen(false)} />
+            <header id="main-header">
+                <div id="title">
+                    <img src={logo} />
+                    <h1>React Food Order App</h1>
+                </div>
+                <nav>
+                    <Button textOnly={true} onClick={handleCartClick}>
+                        Cart ({totalQuantity})
+                    </Button>
+                </nav>
+            </header>
+        </>
+
     )
 }
 
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,9 +1,53 @@
-const Modal = (props) => {
-    return ( 
-        <dialog>
-            {props.children}
+import React, { useRef, useEffect } from "react";
+import { useCart } from "../../store/CartContext";
+import "../../index.css";
+
+const Modal = ({ isOpen, onClose }) => {
+    const dialogRef = useRef()
+    const { cartItems } = useCart();
+    const [checkoutAlert, setCheckoutAlert] = React.useState("")
+
+    const calculateTotal = () => {
+        return cartItems.reduce((total, item) => {
+            return total + (parseFloat(item.price) * item.quantity);
+        }, 0).toFixed(2);
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            dialogRef.current.showModal()
+        } else {
+            dialogRef.current.close()
+        }
+
+        return () => {
+            if (dialogRef.current?.open) dialogRef.current.close()
+        }
+    }, [isOpen])
+
+    const handleCheckoutClick = () => {
+        setCheckoutAlert("Store does not exist yet!");
+    }
+
+    return (
+        <dialog className="modal cart" ref={dialogRef}>
+            <h2>Cart</h2>
+            <ul>
+                {cartItems.map((item) => (
+                    <li className="cart-item" key={item.id}>
+                        <p>{item.name} - {item.quantity}x</p>
+                    </li>
+                ))}
+            </ul>
+            <p className="cart-total">Total: {calculateTotal()}€</p>
+            {checkoutAlert && (
+                <p style={{ color: "red" }}>{checkoutAlert}</p>
+            )}
+            <p className="modal-actions">
+                <button className="text-button" onClick={onClose}>Close</button>
+                <button className="button" onClick={handleCheckoutClick}>Checkout</button>
+            </p>
         </dialog>
-        
     )
 }
 
